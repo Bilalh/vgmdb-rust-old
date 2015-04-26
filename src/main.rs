5b45ea9c9f94ddcc920152610297325bd889608a
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-04-26 03:08:11 +0100

    read tags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate id3;
+
+use id3::Tag;
+
+fn print_id3_tags(tag:Tag){
+    print!("{}", tag.artist().unwrap() );
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut tag = Tag::read_from_path("mp3s_examples/03 Class__XIO_PROCEED;.mp3");
+
+    let _  = match tag {
+       Ok(t)    =>  print_id3_tags(t),
+       Err(_)   =>  ()
+    };
+
 }
