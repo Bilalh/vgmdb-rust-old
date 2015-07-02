7f01e3d9828a74fca972f05966ae6541c167def8
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-07-02 23:47:18 +0100

    album Metadata

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::fs;
 use std::path::Path;
 use std::ffi::OsStr;
 
-use argparse::{ArgumentParser,StoreFalse, Store};
+use argparse::{ArgumentParser,StoreFalse, Store, StoreTrue};
 
 #[derive(Debug)]
 struct Options {
@@ -19,17 +19,19 @@ struct Options {
     album_id: i32,
     tag_album : bool,
     tag_title : bool,
-    tag_num   : bool
+    tag_num   : bool,
+    whole_album_meta: bool
 }
 
-fn get_args() -> Options { 
+fn get_args() -> Options {
     let mut options = Options {
         length_check: true,
         dir: "".to_string(),
         album_id: 0,
         tag_album: true,
         tag_title: true,
-        tag_num: true
+        tag_num: true,
+        whole_album_meta : false
     };
     {
         let mut ap = ArgumentParser::new();
@@ -43,6 +45,9 @@ fn get_args() -> Options {
         ap.refer(&mut options.length_check)
             .add_option(&["-l", "--no-length-check"], StoreFalse,
             "Continue even if there is mismatch in number of tracks  of the dir and the db");
+        ap.refer(&mut options.whole_album_meta)
+            .add_option(&["-M", "--only-album-meta"], StoreTrue,
+            "Only store album meta data");
         ap.refer(&mut options.tag_album)
             .add_option(&["-a", "--no-tag-album"], StoreFalse,
             "Don't change the album name ");
@@ -69,23 +74,35 @@ fn main() {
         .filter(|x| x.path().extension().and_then(OsStr::to_str) == Some("mp3"))
         .collect();
 
-    let tracks = album.tracks();
-    let tracks_len = tracks.len();
-    let discs_len = album.discs.len();
+    {
+        let tracks = album.tracks();
+        let tracks_len = tracks.len();
+        // let discs_len = album.discs.len();
 
-    println!("Album: {:?}", album );
+        println!("Album: {:?}", album );
 
-    let dir_len = paths.len();
-    if options.length_check && dir_len != tracks_len{
-        panic!("Lengths not equal, tracks {} != dir {} ", tracks_len, dir_len)
+        let dir_len = paths.len();
+        if options.length_check && dir_len != tracks_len{
+            panic!("Lengths not equal, tracks {} != dir {} ", tracks_len, dir_len)
+        }
     }
 
-    for (path,(disc_num,track)) in paths.iter().zip(tracks) {
-        let p = path.path();
+    if options.whole_album_meta{
+        do_per_album_meta(options, &paths, album);
+    }else{
+        do_per_track(options, &paths, album);
+    }
+
+}
+
+fn do_per_album_meta(options:Options, paths:&Vec<std::fs::DirEntry>, album:vgmdb::data::Album){
+    println!("Album Metadata only");
+
+    for entry in paths.iter() {
+
+        let p = entry.path();
         let s = format!("{}", p.display());
         println!("Path: {}", s);
-        println!("Data: {:?}", track );
-
 
         let mut buf = album.category.clone().unwrap_or("".to_string());
         if let Some(o) = album.classification.clone(){
@@ -103,22 +120,6 @@ fn main() {
 
         let mut tag = Tag::read_from_path(p).unwrap();
         println!("\tDoing read tag");
-
-        if options.tag_title{
-            tag.set_title(track.name.clone());
-        }
-        if options.tag_album{
-            tag.set_album(album.name.clone());
-        }
-        println!("\tDoing title album");
-
-        if options.tag_num{
-            tag.set_track(track.index as u32);
-            tag.set_total_tracks(tracks_len as u32);
-            tag.set_disc(disc_num as u32);
-            tag.set_total_discs(discs_len as u32);
-        }
-        println!("\tDoing nums");
 
         if let Some(ref date) = album.release_date{
             set_release_date(&mut tag,date.clone());
@@ -142,10 +143,85 @@ fn main() {
         }
 
         println!("   ");
+
+
     }
 
 }
 
+fn do_per_track(options:Options, paths:&Vec<std::fs::DirEntry>, album:vgmdb::data::Album){
+    println!("Per track metadata");
+
+    let tracks = album.tracks();
+    let tracks_len = tracks.len();
+    let discs_len = album.discs.len();
+
+    for (entry,(disc_num,track)) in paths.iter().zip(tracks) {
+        let p = entry.path();
+        let s = format!("{}", p.display());
+        println!("Path: {}", s);
+        println!("Data: {:?}", track );
+
+
+        let mut buf = album.category.clone().unwrap_or("".to_string());
+        if let Some(o) = album.classification.clone(){
+            buf = format!("{}, {},",buf,o);
+        }else{
+            buf = format!("{},", buf);
+        }
+
+        let comment = format!(
+               "\n{}, vgmdb.net/album/{}, \n{}"
+             , album.catalog.clone().unwrap_or("".to_string())
+             , options.album_id
+             , buf );
+        println!("\tDone making comment");
+
+        let mut tag = Tag::read_from_path(p).unwrap();
+        println!("\tDone read tag");
+
+        if options.tag_title{
+            tag.set_title(track.name.clone());
+        }
+        if options.tag_album{
+            tag.set_album(album.name.clone());
+        }
+        println!("\tDone title album");
+
+        if options.tag_num{
+            tag.set_track(track.index as u32);
+            tag.set_total_tracks(tracks_len as u32);
+            tag.set_disc(disc_num as u32);
+            tag.set_total_discs(discs_len as u32);
+        }
+        println!("\tDone nums");
+
+        if let Some(ref date) = album.release_date{
+            set_release_date(&mut tag,date.clone());
+            let year_str:String = date.chars().take(4).collect();
+
+            if let Ok(year) = year_str.parse(){
+                set_year_id2_3(&mut tag, year);
+            }
+        }
+
+        println!("\tDone date");
+
+        tag.remove_comment(None, None);
+        tag.add_comment("", comment);
+
+        println!("\tDone comment");
+
+        match  tag.save() {
+           Ok(_)  => println!("Processed: {}", s),
+           Err(e) => println!("Failed: {}, {}", s, e)
+        }
+
+        println!("   ");
+    }
+
+
+}
 
 fn set_year_id2_3(t:&mut Tag , year: i32) {
     t.add_text_frame("TDRC", format!("{}", year));
